54b9e6644d983038272ef2d90611cfb31db1c900
jaciteixeira <rm99627@fiap.com.br>
2023-10-05 11:27:54 -0300

    corrigido a classe SerieDao

--- src/main/java/br/com/fiap/seriesapi/data/SerieDao.java
+++ src/main/java/br/com/fiap/seriesapi/data/SerieDao.java
@@ -1,12 +1,13 @@
 package br.com.fiap.seriesapi.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.fiap.seriesapi.model.Serie;
 
 public class SerieDao {
 
-	List<Serie> series = new List<>;
+	List<Serie> series = new ArrayList<>();
 	
 	public SerieDao() {
 		var serie1 = new Serie(
